df66b6441cf374665e97b8a2dc908c18754f480c
PujiWhap <ramirezjustine815@gmail.com>
2026-05-26 23:49:37 +0800

    Added

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,12 +5,20 @@ dotenv.config()
 
 const {Pool} = pkg
 
-export const pool =  new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized : false} : false
-  
-})
-      
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false 
+      }
+    })
+  : new Pool({
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DB
+    })
 
 pool.query('SELECT NOW()',(err,res)=>{
   if(err){
