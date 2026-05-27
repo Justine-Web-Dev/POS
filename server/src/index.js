fc504e71294e5d995d34c4c5800aee6ed1e6b6b1
PujiWhap <ramirezjustine815@gmail.com>
2026-05-27 17:36:57 +0800

    added

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://pos-client-7u1a-git-main-justine-rmairezs-projects.vercel.app'],
+  origin: ['http://localhost:5173', 'https://pos-client-7u1a-j7ztab7aa-justine-rmairezs-projects.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
